e4735e947dd38e1db4048e80fc42072784074027
ParkHaeBeen <130157565+ParkHaeBeen@users.noreply.github.com>
2023-07-17 19:24:48 +0900

    [level 2] Title: 의상, Time: 0.51 ms, Memory: 75.8 MB -BaekjoonHub

--- 프로그래머스/lv2/42578. 의상/의상.java
+++ 프로그래머스/lv2/42578. 의상/의상.java
@@ -5,12 +5,9 @@ class Solution {
         int answer = 0;
         Map<String,Integer> list= new HashMap<>();
         list.put(clothes[0][1], 1);
-        System.out.println(clothes.length);
+
         for (int i=1;i< clothes.length;i++) {
-            for (Map.Entry<String, Integer> stringIntegerEntry : list.entrySet()) {
-                System.out.println("stringIntegerEntry.getKey() = " + stringIntegerEntry.getKey()+i);
-                System.out.println("stringIntegerEntry.getValue() = " + stringIntegerEntry.getValue());
-            }
+
             System.out.println(clothes[i][1]);
             boolean flag=false;
             for (Map.Entry<String, Integer> stringIntegerEntry : list.entrySet()) {
@@ -35,28 +32,14 @@ class Solution {
             arrayList.add(value);
         }
 
-        for (Map.Entry<String, Integer> stringIntegerEntry : list.entrySet()) {
-            System.out.println("stringIntegerEntry.getKey() = " + stringIntegerEntry.getKey());
-            System.out.println("stringIntegerEntry.getValue() = " + stringIntegerEntry.getValue());
-        }
 
-        for (Integer integer : arrayList) {
-            System.out.println("integer = " + integer);
-        }
         for(int i=0;i<arrayList.size();i++){
             int gop=1;
             for(int k=i+1;k<arrayList.size();k++){
                 gop*=(arrayList.get(k)+1);
             }
-            System.out.println("gop = " + gop);
             answer+=arrayList.get(i)*gop;
-            System.out.println("answer = " + answer);
         }
-
-
-       // System.out.println("answer = " + answer);
-
-
 
         return answer;
     }
