fd8f2bd5a8b440fad67f9078efb0a19022a62af2
ParkHaeBeen <130157565+ParkHaeBeen@users.noreply.github.com>
2023-07-28 17:02:49 +0900

    [unrated] Title: 햄버거 만들기, Time: 0.16 ms, Memory: 75.5 MB -BaekjoonHub

--- 프로그래머스/unrated/133502. 햄버거 만들기/햄버거 만들기.java
+++ 프로그래머스/unrated/133502. 햄버거 만들기/햄버거 만들기.java
@@ -1,24 +1,22 @@
 import java.util.*;
 class Solution {
     public int solution(int[] ingredient) {
- 		int result = 0;
-		Stack<Integer> stack = new Stack<>();
-		for (int in : ingredient) {
-			stack.push(in);
-			if (stack.size() >= 4) {
-				int size = stack.size();
-				if(stack.get(size - 1) == 1
-						&& stack.get(size - 2) == 3
-						&& stack.get(size - 3) == 2
-						&& stack.get(size - 4) == 1) {
-					result++;
-					stack.pop();
-					stack.pop();
-					stack.pop();
-					stack.pop();
-				}
-			}
-		}
-		return result;
+        int answer = 0;
+        Stack<Integer> list= new Stack<>();
+        for(int gre:ingredient){
+            list.add(gre);
+            
+            if(list.size()>=4){
+                if(list.get(list.size()-1)==1&&list.get(list.size()-2)==3&&list.get(list.size()-3)==2&&
+                  list.get(list.size()-4)==1){
+                    for(int i=0;i<4;i++){
+                        list.pop();
+                        
+                    }
+                    answer++;
+                }
+            }
+        }
+        return answer;
     }
 }
