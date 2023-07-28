92ebec071a03ae7e3e5fdc6a90b0ac45796bd1e3
ParkHaeBeen <130157565+ParkHaeBeen@users.noreply.github.com>
2023-07-29 08:58:36 +0900

    [level 1] Title: 최소직사각형, Time: 1.12 ms, Memory: 85 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/86491. 최소직사각형/최소직사각형.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int solution(int[][] sizes) {
+        int answer = 0;
+        for(int i=0;i<sizes.length;i++){
+            if(sizes[i][0]<sizes[i][1]){
+                int tmp=sizes[i][0];
+                sizes[i][0]=sizes[i][1];
+                sizes[i][1]=tmp;
+            }
+        }
+        int maxW=0;
+        int maxH=0;
+        
+        for(int i=0;i<sizes.length;i++){
+            if(maxW<sizes[i][0]){
+                maxW=sizes[i][0];
+            }
+            
+            if(maxH<sizes[i][1]){
+                maxH=sizes[i][1];
+            }
+        }
+        
+        answer=maxW*maxH;
+        return answer;
+    }
+}
