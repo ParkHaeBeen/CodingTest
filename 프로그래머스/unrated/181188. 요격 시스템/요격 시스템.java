c0037146e51b74b88c0b6183d9f31c1d44553df1
ParkHaeBeen <130157565+ParkHaeBeen@users.noreply.github.com>
2023-08-13 10:47:00 +0900

    [unrated] Title: 요격 시스템, Time: 0.34 ms, Memory: 75 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181188. 요격 시스템/요격 시스템.java
@@ -0,0 +1,36 @@
+import java.util.*;
+import java.io.*;
+class Solution {
+    public int solution(int[][] targets) {
+        int answer = 1;
+        int max=0;
+         Arrays.sort(targets, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                if(o1[0]==o2[0]){
+                    return o1[1]-o2[1];
+                }
+                return o1[0]-o2[0];
+            }
+        });
+    
+        int start=targets[0][0];
+        int end=targets[0][1];
+        for(int i=1;i<targets.length;i++){
+            
+            if(targets[i][0]>=end){
+                answer++;
+                start=targets[i][0];
+                end=targets[i][1];
+            }else if(targets[i][0]<end){
+                start=targets[i][0];
+                if(targets[i][1]<end){
+                    end=targets[i][1];
+                }
+            }
+        }
+        
+        return answer;
+    }
+}
+
