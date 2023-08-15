01e3bcc924855d66432d56ca6e71464279b02e10
ParkHaeBeen <130157565+ParkHaeBeen@users.noreply.github.com>
2023-08-15 21:13:44 +0900

    [level 1] Title: 소수 찾기, Time: 12.09 ms, Memory: 53.2 MB -BaekjoonHub

--- 프로그래머스/lv1/12921. 소수 찾기/소수 찾기.java
+++ 프로그래머스/lv1/12921. 소수 찾기/소수 찾기.java
@@ -1,30 +1,21 @@
 class Solution {
     public int solution(int n) {
-        if (n < 2) {
-            return 0;
-        }
-
-        boolean[] isPrime = new boolean[n + 1];
-        for (int i = 2; i <= n; i++) {
-            isPrime[i] = true;
-        }
-
-        for (int i = 2; i * i <= n; i++) {
-            if (isPrime[i]) {
-                for (int j = i * i; j <= n; j += i) {
-                    isPrime[j] = false;
+        
+        boolean[] arr=new boolean[n+1];
+        
+        for(int i=2;i*i<=n;i++){
+            if(arr[i]==false) {
+                for (int k =i* i; k <= n; k += i) {
+                    arr[k]=true;
                 }
             }
         }
-
-        int primeCount = 0;
-        for (int i = 2; i <= n; i++) {
-            if (isPrime[i]) {
-                primeCount++;
+        int answer=0;
+        for (int i=2;i<arr.length;i++) {
+            if(arr[i]==false){
+                answer++;
             }
         }
-
-        return primeCount;
-    
+        return answer;
     }
 }
