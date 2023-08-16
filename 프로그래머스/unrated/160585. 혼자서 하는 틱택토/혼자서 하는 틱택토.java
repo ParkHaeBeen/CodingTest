384c0b61be4b1d7565705fe92c624df45da05997
ParkHaeBeen <130157565+ParkHaeBeen@users.noreply.github.com>
2023-08-16 11:22:49 +0900

    [unrated] Title: 혼자서 하는 틱택토, Time: 14.42 ms, Memory: 65.1 MB -BaekjoonHub

--- 프로그래머스/unrated/160585. 혼자서 하는 틱택토/혼자서 하는 틱택토.java
+++ 프로그래머스/unrated/160585. 혼자서 하는 틱택토/혼자서 하는 틱택토.java
@@ -1,74 +1,55 @@
+import java.util.Arrays;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
 class Solution {
-    static char[][] bd;
-    static int[] dirX = new int[]{0,0,1,-1,1,-1,1,-1};
-    static int[] dirY = new int[]{1,-1,0,0,1,1,-1,-1};
-    static boolean owin;
-    static boolean xwin;
-
+    private static final Character O = 'O';
+    private static final Character X = 'X';
     public int solution(String[] board) {
-        int answer = 0;
-        bd = new char[board.length][board[0].length()];
-        int idx = 0;
-        for (String bb : board) {
-            for (int i = 0; i < bb.length(); i++) {
-                char c = bb.charAt(i);
-                bd[idx][i] = c;
-            }
-            idx++;
+        Map<Character, Long> counts = Arrays.stream(board)
+                .flatMap(row -> row.chars()
+                        .mapToObj(word -> (char) word))
+                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+        int xCount = counts.getOrDefault(X, 0L).intValue();
+        int oCount = counts.getOrDefault(O, 0L).intValue();
+
+        if (xCount > oCount || oCount - xCount > 1) {
+            return 0;
         }
 
-        int ocnt = 0;
-        int xcnt = 0;
-        for (int i = 0; i < bd.length; i++) {
-            for (int k = 0; k < bd.length; k++) {
-                if (bd[i][k] == 'O') {
-                    find(k, i, bd[i][k]);
-                    ocnt++;
-                } else if (bd[i][k] == 'X') {
-                    find(k, i, bd[i][k]);
-                    xcnt++;
-                }
-            }
-        }
-        
-        if (owin && xwin) return answer;
-        else if (owin) {
-            if (xcnt == ocnt - 1) answer = 1;
-        } else if (xwin) {
-            if (ocnt == xcnt) answer = 1;
-        } else {
-            if (ocnt == xcnt || ocnt == xcnt + 1) {
-                answer = 1;
-            }
+        boolean xWin = checkWin(board, X);
+        boolean oWin = checkWin(board, O);
+
+        if (xWin && oWin) {
+            return 0;
         }
 
-        return answer;
+        if (xWin && oCount > xCount ||
+                oWin && xCount >= oCount) {
+            return 0;
+        }
+
+        return 1;
     }
 
-    private static void find(int x, int y, char c) {
-        for (int i = 0; i < 8; i++) {
-            int cnt = 1;  // Start with 1 considering the starting point itself
-            int newX = x;
-            int newY = y;
-            while (true) {
-                newX = newX + dirX[i];
-                newY = newY + dirY[i];
-                if (newX < 0 || newY < 0 || newY >= 3 || newX >= 3) break;
-                if (bd[newY][newX] == c) {
-                    cnt++;
-                } else {
-                    break;
-                }
-            }
-
-            if (cnt == 3) {
-                if (c == 'O') {
-                    owin = true;
-                } else {
-                    xwin = true;
-                }
-                return; 
+    private boolean checkWin(String[] board, char symbol) {
+        for (int i = 0; i < 3; i++) {
+            if (board[i].charAt(0) == symbol && board[i].charAt(1) == symbol && board[i].charAt(2) == symbol) {
+                return true;
             }
         }
+
+        for (int i = 0; i < 3; i++) {
+            if (board[0].charAt(i) == symbol && board[1].charAt(i) == symbol && board[2].charAt(i) == symbol) {
+                return true;
+            }
+        }
+
+        if (board[0].charAt(0) == symbol && board[1].charAt(1) == symbol && board[2].charAt(2) == symbol) {
+            return true;
+        }
+
+        return board[0].charAt(2) == symbol && board[1].charAt(1) == symbol && board[2].charAt(0) == symbol;
     }
 }
