84518630990de5d550f70c495beb1d434557cc29
ParkHaeBeen <130157565+ParkHaeBeen@users.noreply.github.com>
2023-08-18 15:24:34 +0900

    [unrated] Title: 뒤에 있는 큰 수 찾기, Time: 117.49 ms, Memory: 188 MB -BaekjoonHub

--- 프로그래머스/unrated/154539. 뒤에 있는 큰 수 찾기/README.md
+++ 프로그래머스/unrated/154539. 뒤에 있는 큰 수 찾기/README.md
@@ -1,10 +1,10 @@
 # [unrated] 뒤에 있는 큰 수 찾기 - 154539 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/154539#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/154539) 
 
 ### 성능 요약
 
-메모리: 158 MB, 시간: 21.35 ms
+메모리: 188 MB, 시간: 117.49 ms
 
 ### 구분
 
--- 프로그래머스/unrated/154539. 뒤에 있는 큰 수 찾기/뒤에 있는 큰 수 찾기.java
+++ 프로그래머스/unrated/154539. 뒤에 있는 큰 수 찾기/뒤에 있는 큰 수 찾기.java
@@ -1,38 +1,21 @@
 import java.util.*;
 class Solution {
     public int[] solution(int[] numbers) {
-        int[] answer = new int[numbers.length];
-        int idx=numbers.length-2;
-        answer[numbers.length-1]=-1;
-        ArrayDeque<Integer> list=new ArrayDeque<>();
-        int max=numbers[numbers.length-1];
-        while(idx>=0){
-            if(numbers[idx]<max){
-                answer[idx]=max;
-                if(idx>=1&&numbers[idx]>numbers[idx-1]){
-                    list.add(max);
-                    max=numbers[idx];
-                }
-            }else {
-                int idx2=list.size()-1;
-                if(list.size()!=0) {
-                    while (idx2>=0) {
-                        Integer integer = list.getLast();
-                        if (integer > numbers[idx]) {
-                            answer[idx] = integer;
-                            break;
-                        }
-                        list.pollLast();
-                        
-                        idx2--;
-                    }
-                }
-                if(answer[idx]==0) {
-                    answer[idx] = -1;
-                }
-                max=numbers[idx];
+ int[] answer = new int[numbers.length];
+        Arrays.fill(answer, -1);
+        Stack<Integer> s = new Stack<>();
+        s.push(0);
+        for(int i = 1; i < numbers.length; i++){
+            while(!s.isEmpty()){
+                int idx = s.pop();
+                if(numbers[i] > numbers[idx]){ // 뒤가 더 클때
+                    answer[idx] = numbers[i];
+                } else { // 앞이 더 크거나 같을 때
+                    s.push(idx);
+                    break;
+                } 
             }
-            idx--;
+            s.push(i);
         }
 
         return answer;
