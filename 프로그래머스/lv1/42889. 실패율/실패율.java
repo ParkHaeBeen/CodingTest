9362aeea99940b04d027b4f2e49193f3d6f3c144
ParkHaeBeen <130157565+ParkHaeBeen@users.noreply.github.com>
2023-08-20 13:17:31 +0900

    [level 1] Title: 실패율, Time: 0.82 ms, Memory: 73.5 MB -BaekjoonHub

--- 프로그래머스/lv1/42889. 실패율/실패율.java
+++ 프로그래머스/lv1/42889. 실패율/실패율.java
@@ -1,74 +1,67 @@
 import java.util.*;
-class Solution {
-     public static int[] solution(int N, int[] stages) {
-        int[] result=new int[N];
-        ArrayDeque<Integer> list=new ArrayDeque<>();
-        Arrays.sort(stages);
-        for (int stage : stages) {
-            list.add(stage);
-        }
 
-        List<Game> list2=new ArrayList<>();
-        int idx=1;
-        while(idx<=N){
-            if(idx>N){
+class Solution {
+    public static int[] solution(int N, int[] stages) {
+        int[] answer = new int[N];
+
+        int allplayers=stages.length;
+        Arrays.sort(stages);
+        LinkedList<Stage> list=new LinkedList<>();
+        for(int i=0;i<stages.length;i++){
+            if(stages[i]>N){
                 break;
             }
-            //System.out.println(list.getFirst());
-            if(list.isEmpty()||idx!=list.getFirst()){
-                list2.add(new Game(idx,0));
-            } else if(idx==list.getFirst()) {
-                int all = list.size();
-                int stage = list.pollFirst();
-                int count = 1;
-                while (!list.isEmpty()&&list.getFirst() == stage) {
-                    Integer integer = list.pollFirst();
-                    count++;
-
-                }
-                list2.add(new Game(stage, (double) count / all));
-
+            int nowStage=1;
+            while(i<stages.length-1&&stages[i]==stages[i+1]){
+                nowStage++;
+                i++;
             }
-            idx++;
+            list.add(new Stage(stages[i],(double) nowStage/allplayers));
+
+            allplayers-=nowStage;
         }
 
-        Collections.sort(list2);
+        Collections.sort(list);
 
-
-        for (int i=0;i<result.length;i++) {
-            result[i]=list2.get(i).getStage();
+        boolean[] check=new boolean[N+1];
+        int idx=0;
+        for (Stage stage : list) {
+            answer[idx++]=stage.stage;
+            check[stage.stage]=true;
+        }
+        
+        for(int i=1;i<check.length;i++){
+            if(check[i]==false){
+                answer[idx++]=i;
+            }
         }
 
-        return result;
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        int[] arr=new int[]{4,4,4,4,4};
+        solution(4,arr);
     }
 }
-class Game implements Comparable<Game>{
+
+class Stage implements Comparable<Stage>{
     int stage;
     double fail;
 
-    public int getStage() {
-        return stage;
-    }
-
-    public double getFail() {
-        return fail;
-    }
-
-    public Game(int stage, double fail) {
-        this.stage = stage;
-        this.fail = fail;
+    public Stage(int stage,double fail){
+        this.stage=stage;
+        this.fail=fail;
     }
 
     @Override
-    public int compareTo(Game o) {
-        if (fail > o.fail ) {
-            return -1;
-        }
-        
-        if (fail < o.fail ) {
+    public int compareTo(Stage st){
+        if(st.fail-this.fail>0){
             return 1;
+        }else if(st.fail==this.fail){
+            return this.stage-st.stage;
         }
-        return Integer.compare(stage, o.stage);
+        return -1;
     }
-    
+
 }
