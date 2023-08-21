d4b23cc4d64d81508386c4620a5ccc792d73d7b0
ParkHaeBeen <130157565+ParkHaeBeen@users.noreply.github.com>
2023-08-21 13:48:19 +0900

    [Gold IV] Title: 타임머신, Time: 308 ms, Memory: 19588 KB -BaekjoonHub

--- 백준/Gold/11657. 타임머신/타임머신.java
+++ 백준/Gold/11657. 타임머신/타임머신.java
@@ -1,88 +1,73 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.*;
 
-public class Main {
-    static int N,K;
-    static ArrayList<ArrayList<Node>> list =new ArrayList<>();
-    static long[] arr;
-    static boolean answer=false;
+class Main {
+
+    static ArrayList<Bus> list=new ArrayList<>();
+    static long[] dis;
+    static boolean answer;
     public static void main(String[] args) throws IOException {
-
         BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st=new StringTokenizer(br.readLine());
 
-        StringTokenizer st= new StringTokenizer(br.readLine());
-        N=Integer.parseInt(st.nextToken());
-        K=Integer.parseInt(st.nextToken());
-        arr=new long[N+1];
-
-        Arrays.fill(arr,Integer.MAX_VALUE);
-        for(int i=0;i<=N;i++){
-            list.add(new ArrayList<>());
-        }
-        for(int i=0;i<K;i++){
+        int N=Integer.parseInt(st.nextToken());
+        int M=Integer.parseInt(st.nextToken());
+        dis=new long[N+1];
+        Arrays.fill(dis,Integer.MAX_VALUE);
+        dis[1]=0;
+        for(int i=0;i<M;i++){
             st=new StringTokenizer(br.readLine());
             int start=Integer.parseInt(st.nextToken());
             int end=Integer.parseInt(st.nextToken());
-            int cost=Integer.parseInt(st.nextToken());
+            int time=Integer.parseInt(st.nextToken());
 
-            list.get(start).add(new Node(end,cost));
+            list.add(new Bus(start,end, time));
         }
 
-        arr[1]=0;
+        findMin(N,M);
 
-        solution();
-
-        if(answer){
+        if(answer==true){
             System.out.println(-1);
-        }else {
-            for (int i=2;i<=N;i++) {
-                if(arr[i]==Integer.MAX_VALUE){
+        }else{
+            for(int i=2;i<=N;i++){
+                if(dis[i]==Integer.MAX_VALUE){
                     System.out.println(-1);
                 }else{
-                    System.out.println(arr[i]);
+                    System.out.println(dis[i]);
                 }
             }
         }
-
+        br.close();
     }
 
-    private static void solution() {
-        // N-1번만큼 각 노드에 대한 relaxation을 수행
-        for (int i = 0; i < N - 1; i++) {
-            for (int j = 1; j <= N; j++) { // 정점은 1부터 N까지이므로
-                for (Node edge : list.get(j)) {
-                    if (arr[j] != Integer.MAX_VALUE && arr[edge.end] > arr[j] + edge.cost) {
-                        arr[edge.end] = arr[j] + edge.cost;
-                    }
+    private static void findMin(int n,int m) {
+
+        for(int i=0;i<n;i++){
+            for(int k=0;k<m;k++){
+                Bus bus = list.get(k);
+                if(dis[bus.start]!=Integer.MAX_VALUE&&dis[bus.end]>dis[bus.start]+bus.time){
+                    dis[bus.end]=dis[bus.start]+bus.time;
                 }
             }
         }
 
-        // 음수 사이클 검사
-        for (int j = 1; j <= N; j++) {
-            for (Node edge : list.get(j)) {
-                if (arr[j] != Integer.MAX_VALUE && arr[edge.end] > arr[j] + edge.cost) {
-                    answer=true;
-                    return;
-                }
+        for(int i=0;i<m;i++){
+            Bus bus = list.get(i);
+            if(dis[bus.start]!=Integer.MAX_VALUE&&dis[bus.end]>dis[bus.start]+bus.time){
+                answer=true;
+                break;
             }
         }
     }
 
 }
 
-class Node implements Comparable<Node>{
-    int end, cost;
+class Bus{
+    int start,end,time;
 
-    public Node(int end, int cost) {
+    public Bus(int start, int end, int time) {
+        this.start = start;
         this.end = end;
-        this.cost = cost;
-    }
-
-    @Override
-    public int compareTo(Node o) {
-        return this.cost-o.cost;
+        this.time = time;
     }
 }
