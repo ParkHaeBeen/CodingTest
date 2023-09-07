206af738c9e833620df6486eed7440bb1f7093a6
ParkHaeBeen <130157565+ParkHaeBeen@users.noreply.github.com>
2023-09-07 20:42:33 +0900

    [unrated] Title: 이모티콘 할인행사, Time: 0.04 ms, Memory: 72.2 MB -BaekjoonHub

--- 프로그래머스/unrated/150368. 이모티콘 할인행사/README.md
+++ 프로그래머스/unrated/150368. 이모티콘 할인행사/README.md
@@ -1,10 +1,10 @@
 # [unrated] 이모티콘 할인행사 - 150368 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/150368) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/150368#qna) 
 
 ### 성능 요약
 
-메모리: 75 MB, 시간: 0.04 ms
+메모리: 72.2 MB, 시간: 0.04 ms
 
 ### 구분
 
--- 프로그래머스/unrated/150368. 이모티콘 할인행사/이모티콘 할인행사.java
+++ 프로그래머스/unrated/150368. 이모티콘 할인행사/이모티콘 할인행사.java
@@ -1,57 +1,54 @@
-import java.util.Arrays;
-
 class Solution {
-
-    private static final int[] RATE = {90, 80, 70, 60};
-    private static int EMOTICON_PLUS = 0;
-    private static int TOTAL_SALES = 0;
-
+static double[] rate={60,70,80,90};
+    static int[] com;
+    static int[][] user;
+    static int realusers,moneysum;
     public int[] solution(int[][] users, int[] emoticons) {
-        getPrices(emoticons, users, 0, new int[emoticons.length]);
-        return new int[]{EMOTICON_PLUS, TOTAL_SALES};
+        int[] answer = new int[2];
+        user=users;
+        com=new int[emoticons.length];
+        getCombi(emoticons,0);
+        answer[0]=realusers;
+        answer[1]=moneysum;
+        return answer;
     }
 
-    private void getPrices(int[] emoticons, int[][] users, int cur, int[] rates) {
-        if (cur == emoticons.length) {
-            updateAnswer(emoticons, users, rates);
+    private void getCombi(int[] emoticons, int L) {
+        if(L==emoticons.length){
+            getMax(com,emoticons);
             return;
         }
-
-        for (int rate : RATE) {
-            rates[cur] = rate;
-            getPrices(emoticons, users, cur + 1, rates);
+        
+        for(int i=0;i<rate.length;i++){
+            com[L]=i;
+            getCombi(emoticons,L+1);
         }
     }
 
-    private void updateAnswer(int[] emoticons, int[][] users, int[] rates) {
-
-        int ePlus = 0;
-        int totalExpense = 0;
-
-        for (int[] user : users) {
-            int expense = 0;
-            int rate = user[0];
-            int price = user[1];
-            for (int i = 0; i < rates.length; i++) {
-                if (100 - rates[i] >= rate) {
-                    expense += emoticons[i] * rates[i] / 100;
-                }
-                if (expense >= price) {
-                    ePlus += 1;
-                    expense = 0;
-                    break;
+    private void getMax(int[] com, int[] emoticons) {
+        int joinuser=0;
+        int totalmoney=0;
+        for (int[] ints : user) {
+            int sum=0;
+            for(int i=0;i<com.length;i++){
+                if(ints[0]<=(100-rate[com[i]])){
+                    sum+=emoticons[i]*rate[com[i]]/100;
                 }
             }
-            totalExpense += expense;
+            
+            if(sum>=ints[1]){ 
+                joinuser++;
+            }else{
+                totalmoney+=sum;
+            }
         }
-
-        if (ePlus > EMOTICON_PLUS) {
-            EMOTICON_PLUS = ePlus;
-            TOTAL_SALES = totalExpense;
-        } else if (ePlus == EMOTICON_PLUS) {
-            TOTAL_SALES = Math.max(totalExpense, TOTAL_SALES);
+        if(joinuser>realusers){
+            realusers=joinuser;
+            moneysum=totalmoney;
+        }else if(joinuser==realusers){
+            if(totalmoney>moneysum){
+                moneysum=totalmoney;
+            }
         }
     }
-
-
 }
