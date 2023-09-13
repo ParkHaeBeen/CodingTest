c7e3a036bc39ef76457c6994f24d5ca8d1ade412
ParkHaeBeen <130157565+ParkHaeBeen@users.noreply.github.com>
2023-09-13 14:00:14 +0900

    [unrated] Title: 디펜스 게임, Time: 9.53 ms, Memory: 77.3 MB -BaekjoonHub

--- 프로그래머스/unrated/142085. 디펜스 게임/디펜스 게임.java
+++ 프로그래머스/unrated/142085. 디펜스 게임/디펜스 게임.java
@@ -1,25 +1,49 @@
 import java.util.*;
+import java.util.stream.Collectors;
 class Solution {
+    static int answer=Integer.MIN_VALUE;
     public int solution(int n, int k, int[] enemy) {
-        int answer = enemy.length;
-        Queue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
-
-        int my = n;
-        int card = k;
-        for (int i = 0; i < enemy.length; i++) {
-            my -= enemy[i];
-            pq.add(enemy[i]);
-
-            if (my < 0) {
-                if (card > 0 && !pq.isEmpty()) {
-                    my += pq.poll();
-                    card--;
-                } else {
-                    answer = i;
-                    break;
-                }
+        if(k>=enemy.length){
+            return enemy.length;
+        }
+        getMaxRound(n,k,enemy,0,enemy.length);
+        return answer;
+    }
+    public void getMaxRound(int n,int k, int[] enemy,int start,int end){
+       
+        
+        while(start<end){
+             int mid=(start+end)/2;
+            if(possibleDefence(mid,enemy,k,n)){
+                start=mid+1;
+            }else{
+               end=mid;
             }
         }
-        return answer;
+        answer=start;
+        
+    }
+    
+    public boolean possibleDefence(int mid,int [] enemy,int k,int n){
+         List<Integer> collect = Arrays.stream(enemy, 0, mid + 1)
+                .boxed()
+                .sorted()
+                .collect(Collectors.toList());
+
+        int nn = n;
+
+        int size = collect.size();
+
+        for (int i = 0; i < size; i++) {
+            Integer el = collect.get(i);
+            if (el <= nn) {
+                nn -= el;
+                continue;
+            }
+         
+            return k >= size - i; 
+        }
+        return true;
+        
     }
 }
