775aec020528d5378de75e533c053b114a6430ff
ParkHaeBeen <130157565+ParkHaeBeen@users.noreply.github.com>
2023-09-18 20:33:29 +0900

    [Silver V] Title: 단어 정렬, Time: 1908 ms, Memory: 25896 KB -BaekjoonHub

---
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.*;
+public class Main{
+    public static void main(String[] args) throws IOException{
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        int N=Integer.parseInt(br.readLine());
+        List<String> list=new ArrayList<>();
+        for(int i=0;i<N;i++){
+            String arr=br.readLine();
+            if(list.contains(arr)) continue;
+            list.add(arr);
+        }
+        Collections.sort(list, new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                if(o1.length()==o2.length()){
+                    return o1.compareTo(o2);
+                }
+                return o1.length()-o2.length();
+            }
+        });
+
+        for (String s : list) {
+            System.out.println(s);
+        }
+    }
+}
