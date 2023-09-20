57b65264d9ea1a28e1e79546b16357d2498c1947
ParkHaeBeen <130157565+ParkHaeBeen@users.noreply.github.com>
2023-09-21 08:02:58 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 1.03 ms, Memory: 76.3 MB -BaekjoonHub

--- 프로그래머스/lv2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
+++ 프로그래머스/lv2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
@@ -1,58 +1,40 @@
 import java.util.*;
 class Solution {
-    static ArrayList<ArrayList<Integer>> list=new ArrayList<>();
+    static ArrayList<ArrayList<Integer>> list =new ArrayList<>();
+    static int count;
+    static int answer=Integer.MAX_VALUE;
     public static int solution(int n, int[][] wires) {
-        int answer = -1;
         for(int i=0;i<=n;i++){
             list.add(new ArrayList<>());
         }
-        for (int[] wire : wires) {
-            list.get(wire[0]).add(wire[1]);
-            list.get(wire[1]).add(wire[0]);
+
+        for(int[] w:wires){
+            list.get(w[0]).add(w[1]);
+            list.get(w[1]).add(w[0]);
         }
 
-        int min = Integer.MAX_VALUE;
-        int mid=n/2;
-        for(int i=0;i<wires.length;i++){
+
+        for(int[] w:wires){
             boolean[] visited=new boolean[n+1];
-            int v1=wires[i][0];
-            int v2=wires[i][1];
-            list.get(v1).remove(Integer.valueOf(v2));
-            list.get(v2).remove(Integer.valueOf(v1));
+            visited[w[0]]=true;
+            count=1;
+            visited[w[1]]=true;
+            getCount(w[1],visited,0,0);
+            int one=n-count;
+            answer=Math.min(answer,Math.abs(one-count));
 
-            int group = getGroup(1, visited);
-
-            int diff=Math.abs(group-(n-group));
-
-            min=Math.min(min,diff);
-            list.get(v1).add(v2);
-            list.get(v2).add(v1);
         }
-        return min;
+        return answer;
     }
 
-    private static int getGroup(int idx, boolean[] visited) {
-        ArrayDeque<Integer> list2 = new ArrayDeque<>();
-        list2.add(idx);
-        int count=0;
-        visited[idx]=true;
-        while (true) {
-            int length = list2.size();
-            for (int i = 0; i < length; i++) {
-                Integer num = list2.pollFirst();
-                ArrayList<Integer> arrayList = list.get(num);
-                for (Integer integer : arrayList) {
-                    if (!visited[integer]) {
-                        visited[integer]=true;
-                        list2.add(integer);
-                    }
-                }
+    public static void getCount(int start,boolean[] visited,int cnt,int L){
+        for(int i=0;i<list.get(start).size();i++){
+            int num=list.get(start).get(i);
+            if(!visited[num]){
+                visited[num]=true;
                 count++;
-            }
-            if (length==0){
-                break;
+                getCount(num,visited,cnt++,L+1);
             }
         }
-        return count;
     }
 }
