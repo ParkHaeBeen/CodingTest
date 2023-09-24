5d5dd2bc83299d4693084a6c091a9615239a7f70
ParkHaeBeen <130157565+ParkHaeBeen@users.noreply.github.com>
2023-09-24 17:55:11 +0900

    [level 2] Title: 2개 이하로 다른 비트, Time: 156.46 ms, Memory: 136 MB -BaekjoonHub

--- 프로그래머스/lv2/77885. 2개 이하로 다른 비트/2개 이하로 다른 비트.java
+++ 프로그래머스/lv2/77885. 2개 이하로 다른 비트/2개 이하로 다른 비트.java
@@ -1,28 +1,30 @@
 class Solution {
-    public long[] solution(long[] numbers) {
-long[] answer = new long[numbers.length];
-        int answeridx=0;
-        for (long number : numbers) {
-            String binaryString = Long.toBinaryString(number);
-            int idx=-1;
-            for(int i=binaryString.length()-1;i>=0;i--){
-                if(binaryString.charAt(i)=='0'){
-                    idx=i;
-                    break;
-                }
-            }
-            
-            if(number%2==0){
-                answer[answeridx++]=number+1;
+public static long[] solution(long[] numbers) {
+        long[] answer = new long[numbers.length];
+        int index=0;
+        for (long num : numbers) {
+            String nums = Long.toBinaryString(num);
+            if(nums.indexOf("0")==-1){
+                nums="10"+nums.substring(1,nums.length());
+            }else if(nums.indexOf("1",1)==-1){
+                nums=nums.substring(0,nums.length()-1)+"1";
             }else{
-                String result;
-                if(idx==-1){
-                    result="10"+binaryString.substring(1,binaryString.length());
-                }else{
-                    result=binaryString.substring(0,idx)+"10"+binaryString.substring(idx+2);
+                int idx = nums.lastIndexOf("01");
+                String num2=nums;
+                if(idx!=-1){
+                    nums=nums.substring(0,idx)+"10"+nums.substring(idx+2);
                 }
-                answer[answeridx++]=Long.parseLong(result,2);
+                
+                idx=nums.lastIndexOf("0");
+                num2=num2.substring(0,idx)+"1"+num2.substring(idx+1);
+                long l2=Long.parseLong(num2,2);
+                long l1=Long.parseLong(nums,2);
+                if(l2>num&&l2<l1||num>=l1){
+                    nums=num2;
+                }
+                
             }
+            answer[index++]=Long.parseLong(nums,2);
         }
         return answer;
     }
