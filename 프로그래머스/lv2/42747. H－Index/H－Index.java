4a94607fc6afdd9693da5eed6de820b07e8d2bf2
ParkHaeBeen <130157565+ParkHaeBeen@users.noreply.github.com>
2023-09-25 13:18:38 +0900

    [level 2] Title: H-Index, Time: 0.40 ms, Memory: 69.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42747. H－Index/H－Index.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class Solution {
+    public int solution(int[] citations) {
+        int answer = 0;
+        Arrays.sort(citations);
+        for(int i=0;i<citations.length;i++){
+            int idx=citations[i];
+            if(citations.length-i<=idx){
+                answer=citations.length-i;
+                break;
+            }
+        }
+        return answer;
+    }
+}
